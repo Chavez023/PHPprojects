25842defc9cb6303b298d29a1fee46c793c0a7b3
Chavez <nimbus.arc@outlook.com>
2023-10-10 18:58:46 +0800

    GIT Commands

---
+++ Git commands handson.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GIT Commands Handson</title>
+</head>
+<body>
+    <?php
+        echo "1. git init<br>";
+        echo "2. git clone &lt;repository_url&gt;<br>";
+        echo "3. git add &lt;file1&gt; &lt;file2&gt; ...<br>";
+        echo "4. git commit -m &quot;Commit message here&quot;<br>";
+        echo "5. git status<br>";
+        echo "6. git log<br>";
+        echo "7. git branch<br>";
+        echo "8. git checkout &lt;branch_name&gt;<br>";
+        echo "9. git pull origin &lt;branch_name&gt;<br>";
+        echo "10. git push origin &lt;branch_name&gt;<br>";
+        echo "11. git remote -v<br>";
+        echo "12. git merge &lt;branch_name&gt;<br>";
+        echo "13. git diff<br>";
+        echo "14. git reset &lt;file&gt;<br>";
+        echo "15. git stash<br>";
+        echo "16. git stash list<br>";
+        echo "17. git stash apply<br>";
+        ?>
+</body>
+</html>
